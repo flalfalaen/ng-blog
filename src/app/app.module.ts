ff643ac2235c337405e6b02184ec2633fca57ba7
alex <falfalaen@gmail.com>
2019-11-01 16:43:07 +0200

    added main and shared components #1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { MainComponent } from './pages/main/main.component';
+import { NewsComponent } from './pages/news/news.component';
+import { PostComponent } from './pages/post/post.component';
+import { AboutComponent } from './pages/about/about.component';
+import { AdminDashboardComponent } from './pages/admin-dashboard/admin-dashboard.component';
+import { UserCabinetComponent } from './pages/user-cabinet/user-cabinet.component';
+import { PopupComponent } from './shared/popup/popup.component';
+import { LoaderComponent } from './shared/loader/loader.component';
+import { LogInSignInFormComponent } from './shared/log-in-sign-in-form/log-in-sign-in-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    NewsComponent,
+    PostComponent,
+    AboutComponent,
+    AdminDashboardComponent,
+    UserCabinetComponent,
+    PopupComponent,
+    LoaderComponent,
+    LogInSignInFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/log-in-sign-in-form/log-in-sign-in-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-log-in-sign-in-form',
+  templateUrl: './log-in-sign-in-form.component.html',
+  styleUrls: ['./log-in-sign-in-form.component.scss']
+})
+export class LogInSignInFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
