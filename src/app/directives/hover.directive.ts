b03f42c16a69cf5800b7059f38aa3424a2e387f0
alex <falfalaen@gmail.com>
2019-11-06 17:39:16 +0200

    directive fix

--- src/app/directives/hover.directive.ts
+++ src/app/directives/hover.directive.ts
@@ -4,17 +4,26 @@ import {Directive, ElementRef, HostListener, Renderer2} from '@angular/core';
   selector: '[appHover]'
 })
 export class HoverDirective {
+  public shadowBox = ' border media shadow m-1 bg-white rounded ';
 
   constructor(private renderer: Renderer2,
               private elementRef: ElementRef) { }
 
   @HostListener('mouseenter') mouseEnter() {
-    this.renderer.addClass(this.elementRef.nativeElement,  'bg-info');
-    this.renderer.addClass(this.elementRef.nativeElement,  'text-white');
+    this.renderer.addClass(this.elementRef.nativeElement,  'border');
+    this.renderer.addClass(this.elementRef.nativeElement,  'media');
+    this.renderer.addClass(this.elementRef.nativeElement,  'shadow');
+    this.renderer.addClass(this.elementRef.nativeElement,  'ml-5');
+    this.renderer.addClass(this.elementRef.nativeElement,  'bg-white');
+    this.renderer.addClass(this.elementRef.nativeElement,  'rounded');
   }
   @HostListener('mouseleave') mouseLeave() {
-    this.renderer.removeClass(this.elementRef.nativeElement, 'bg-info');
-    this.renderer.addClass(this.elementRef.nativeElement,  'text-white');
+    this.renderer.removeClass(this.elementRef.nativeElement,  'border');
+    this.renderer.removeClass(this.elementRef.nativeElement,  'media');
+    this.renderer.removeClass(this.elementRef.nativeElement,  'shadow');
+    this.renderer.removeClass(this.elementRef.nativeElement,  'ml-5');
+    this.renderer.removeClass(this.elementRef.nativeElement,  'bg-white');
+    this.renderer.removeClass(this.elementRef.nativeElement,  'rounded');
   }
 
 }
