89f58dcbd50219ffaa5444824390187aca1132dd
alex <falfalaen@gmail.com>
2019-11-08 18:13:02 +0200

    pipe logic replaced #3

--- src/app/pipes/email-pointer.pipe.ts
+++ src/app/pipes/email-pointer.pipe.ts
@@ -8,8 +8,6 @@ export class EmailPointerPipe implements PipeTransform {
   transform(value: string): string {
     if (value.split(" ").length > 2) {
       return value.split(' ').slice(0, 2).join(' ');
-    } else if (value.split(" ").length > 3) {
-      return value.split(' ').slice(0, 3).join(' ');
     }
   }
 }
