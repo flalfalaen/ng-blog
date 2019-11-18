bfbf2c2d0d069979f2d74f1ebe9095d41486eafd
alex <falfalaen@gmail.com>
2019-11-18 12:59:24 +0200

    lazy loading #3

--- src/app/pages/about/about/about-routing.module.ts
+++ src/app/pages/about/about/about-routing.module.ts
@@ -1,6 +1,7 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {AboutComponent} from "../about.component";
+import {FormsModule} from "@angular/forms";
 
 
 const routes: Routes = [
@@ -9,7 +10,7 @@ const routes: Routes = [
 
 @NgModule({
     declarations: [AboutComponent],
-    imports: [RouterModule.forChild(routes)],
+    imports: [RouterModule.forChild(routes), FormsModule],
     exports: [RouterModule],
 })
 export class AboutRoutingModule {
